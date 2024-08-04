265c360c767d6d71b97785f28eb156fc19fa8a04
Skanda M Shastry <126911189+Sud0-Apt@users.noreply.github.com>
2024-08-04 14:43:17 +0530

    Finalized website

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,8 @@ app.use(cors({
   }));
 
 app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     console.log(req.path, req.method)
     next()
 })
